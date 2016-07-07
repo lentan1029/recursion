3137b461a8bc73c134eea9bf66e103253896a997
lentan <lentan1029@gmail.com>
2016-07-06 22:40:52 -0400

    Completed bare minimum

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var res = [];
+  var start = document.body;
+
+  function filter(node){
+    if(node.classList != undefined){
+      for(var i = 0; i < node.classList.length; i++){
+        if(node.classList[i] == className){
+          res.push(node);
+        }
+      }
+    }
+    for(var j = 0; j < node.childNodes.length; j++){
+      filter(node.childNodes[j]);
+    }
+  }
+
+  filter(start);
+
+  return res;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var res = "";
+  if(typeof obj == "object" && !Array.isArray(obj) && obj != null){
+    res = res + "{";
+    for(var key in obj){
+        if(typeof obj[key] == "function" || typeof obj[key] == "symbol" || obj[key] === undefined){
+          //do nothing
+        } else {
+          res = res + stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+        }
+    }
+    res = res == "{" ? "{}" : res.slice(0,-1) + "}";
+    return res;
+
+  } else if (typeof obj == "string"){
+    return '"' + String(obj) + '"';
+  } else if (Array.isArray(obj)){
+    res = res + "[";
+    for(var i = 0; i < obj.length; i++){
+      if(typeof obj[i] == "function" || typeof obj[i] == "symbol" || obj[i] === undefined){
+        res = res + "null" + ",";
+      } else {
+        res = res + stringifyJSON(obj[i]) + ","
+      }
+    }
+    res = res == "[" ? "[]" : res.slice(0,-1) + "]";
+    return res;
+  } else if (obj === null){
+    return 'null';
+  } else if (typeof obj == "function" || typeof obj == "symbol"){
+    return undefined;
+  } else {
+    return String(obj);
+  }
+
 };
